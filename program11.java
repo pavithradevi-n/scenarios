fab460c734cb051d0b449a904c1c00d38e1c2325
pavithradevi-n <sumathipavithra1234@gmail.com>
2025-12-15 12:44:08 +0530

    add program11

---
+++ program11.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+class SellingPrice {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+    double cp=sc.nextDouble();
+        double profit=sc.nextDouble();
+        
+        System.out.println("Selling Price = " + (cp + profit));
+    }
+}
