68c664898a8bc9afb8d23f5c0534a1d24ca5ac88
pavithradevi-n <sumathipavithra1234@gmail.com>
2025-12-15 12:39:13 +0530

    add program8

---
+++ program8.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+class Cakes {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+   int perHour=sc.nextInt();
+        int hours=sc.nextInt();
+  System.out.println("Total Cakes = " + (perHour * hours));
+    }
+}
+
