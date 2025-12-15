65908a341c8e7a02d1dc5c6ff74ef35fec5c7a9a
pavithradevi-n <sumathipavithra1234@gmail.com>
2025-12-15 12:46:02 +0530

    add program12

---
+++ program12.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+class CostPrice {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+    double sp=sc.nextDouble();
+        double loss=sc.nextDouble();
+System.out.println("Cost Price = " + (sp + loss));
+    }
+}
