add1e9bb799c26153effdaa9c2df0ab78fed170e
pavithradevi-n <sumathipavithra1234@gmail.com>
2025-12-15 12:36:17 +0530

    add program6

---
+++ program6.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+class BytesToBits {
+    public static void main(String[] args) {
+    Scanner sc=new Scanner(System.in);
+int bytes=sc.nextInt();
+        int bits=bytes * 8;
+System.out.println("Bits = " + bits);
+    }
+}
